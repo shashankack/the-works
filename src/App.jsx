c3dd68b74c48c9ff314caed149a14523395327c4
shashankack <shashank.ac.dev@gmail.com>
2025-02-08 14:51:08 +0530

    refactor: simplify routing in App component by removing Suspense wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Suspense, lazy } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Testimonials from "./components/Testimonials/Testimonials";
-
-const Home = lazy(() => import("./components/Home/Home"));
-const NotFound = lazy(() => import("./components/NotFound"));
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="testimonials" element={<Testimonials />} />
-          </Routes>
-        </Suspense>
-      </main>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="testimonials" element={<Testimonials />} />
+      </Routes>
       <Footer />
     </Router>
   );
