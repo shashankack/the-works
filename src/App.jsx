102c6a605df61c31a72df76fd12d9416d91eb150
shashankack <shashank.ac.dev@gmail.com>
2025-02-08 15:09:15 +0530

    feat: implement lazy loading for Home and NotFound components; wrap Routes in Suspense for improved performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Testimonials from "./components/Testimonials/Testimonials";
+
+const Home = lazy(() => import("./components/Home/Home"));
+const NotFound = lazy(() => import("./components/NotFound"));
 
 const App = () => {
   return (
     <Router>
       <Header />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="testimonials" element={<Testimonials />} />
-      </Routes>
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="testimonials" element={<Testimonials />} />
+          </Routes>
+        </Suspense>
+      </main>
       <Footer />
     </Router>
   );
